a10f3e28bb3c82823d5d5442df1a535d44289e02
Akash Yadav <108128604+AkashSatishYadav@users.noreply.github.com>
2023-04-02 20:38:13 +0530

    Add files via upload

---
+++ DependencyInjection/DependencyInjection/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Autofac;
+
+ContainerBuilder builder = new();
+builder.RegisterType<PopularityCalculator>().As<ICalculator>();
+//builder.RegisterType<RankingMapper>().As<IRankingMapper>();
+var container = builder.Build();
+
+List<IStage> stages = new();
+stages.Add(container.ResolveOptional<ICalculator>());
+stages.Add(container.ResolveOptional<IMapper>());
+
+foreach(var stage in stages)
+{
+    stage?.Process();
+}
+
+interface IStage
+{
+    void Process();
+}
+
+interface ICalculator : IStage
+{
+
+}
+
+class PopularityCalculator : ICalculator
+{
+    public void Process()
+    {
+        // Process Something
+    }
+}
+
+interface IMapper : IStage
+{
+
+}
+
+class RankingMapper : IMapper
+{
+    public void Process()
+    {
+        // Process Something
+    }
+}
